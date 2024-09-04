7fcc7514ce16b18e30156c097d2a39cb2ab7c194
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-09-04 20:55:41 +0600

    print added

--- components/global/appointment-cell-action.tsx
+++ components/global/appointment-cell-action.tsx
@@ -2,20 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 
-import { Edit, MoreHorizontal, Trash } from "lucide-react";
-import { useRouter } from "next/navigation";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+import { Printer, Trash } from "lucide-react";
+import { jsPDF } from "jspdf";
 import { useAppContext } from "@/lib/context";
 import api from "@/utils/axiosInstance";
 import { toast } from "sonner";
 import { EditAppointmentModal } from "./edit-appointment-modal";
-
+import { format } from "date-fns";
+import "jspdf-autotable";
 export const AppointmentCellAction: React.FC<any> = ({ data }) => {
   const { setAppointmentRefetch, appointmentRefetch } = useAppContext();
 
@@ -34,8 +28,61 @@ export const AppointmentCellAction: React.FC<any> = ({ data }) => {
     }
   };
 
+  const handlePrint = () => {
+    const doc: any = new jsPDF();
+
+    doc.setFontSize(18);
+    doc.setFont("helvetica", "bold");
+    doc.text("Appointment Details", 105, 20, { align: "center" });
+
+    doc.setFontSize(10);
+    doc.setFont("helvetica", "normal");
+    doc.text(`${data?.doctor?.name}`, 20, 40);
+    doc.text(`${data?.doctor?.speciality}`, 20, 48);
+    doc.text(`${data?.doctor?.hospitalName}`, 20, 56);
+
+    doc.autoTable({
+      startY: 60,
+      head: [["Field", "Value"]],
+      body: [
+        ["Patient Name", data.patientName],
+        ["Phone Number", data.phone],
+        [
+          "Next Appointment Date",
+          format(new Date(data.nextAppointmentDate), "PPP"),
+        ],
+        ["Fee", data.fee],
+      ],
+      styles: {
+        fontSize: 10,
+        halign: "center",
+      },
+      headStyles: { fillColor: [22, 160, 133] },
+      margin: { top: 80 },
+    });
+
+    doc.setFontSize(10);
+    doc.text(
+      "Thank you for choosing our services!",
+      105,
+      doc.internal.pageSize.getHeight() - 20,
+      { align: "center" }
+    );
+
+    doc.autoPrint();
+    doc.output("dataurlnewwindow");
+  };
+
   return (
     <div className="flex items-center gap-3">
+      <Button
+        size="sm"
+        className="h-7 text-xs bg-green-600"
+        onClick={handlePrint}
+      >
+        <Printer className="mr-2 h-4 w-4" /> Print
+      </Button>
+
       <EditAppointmentModal data={data} />
 
       <Button
