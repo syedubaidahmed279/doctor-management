dff0f8b3db040c24814e1ebb951fad5b7fa09da5
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-09-05 21:53:37 +0600

    latest

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://doctor-management-backend.vercel.app/api/v1";
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://doctor-management-backend.vercel.app/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -11,7 +11,7 @@ api.interceptors.request.use(
   (config) => {
     if (typeof window !== "undefined") {
       const token = localStorage.getItem("dmToken");
-      console.log({ token });
+
       if (token) {
         config.headers["Authorization"] = `${token}`;
       }
